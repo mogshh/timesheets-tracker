b84c1934425d1fa95fa5b315817ca7f1f02487e2
Bert Verhelst <verhelstbert@gmail.com>
2024-02-16 08:44:24 +0100

    feat: fix icons and add basic tab detection in chrome extension

--- chrome-extension/src/background/background.js
+++ chrome-extension/src/background/background.js
@@ -1,1 +1,28 @@
-chrome.
+chrome.tabs.onUpdated.addListener(
+  (tabId, changeInfo, tab) => {
+    getTabInfo(tabId);
+  }
+)
+
+chrome.tabs.onActivated.addListener(
+  (info) => {
+    getTabInfo(info.tabId);
+  }
+)
+
+chrome.tabs.onHighlighted.addListener(
+  (info) => {
+    getTabInfo(info.tabIds[0]);
+  }
+)
+
+async function getTabInfo(tabId) {
+  const tabs = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+  console.log({url: tabs[0].url, title: tabs[0].title});
+  fetch({
+    href: 'http://localhost:55588/websites',
+    body: JSON.stringify({
+      
+    })
+  })
+}
